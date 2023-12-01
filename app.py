9b8cb3175b92af7a92e4ba64997db0b8713094cc
Your Name <you@example.com>
2023-12-01 13:12:40 +0100

    prueba

--- app.py
+++ app.py
@@ -77,8 +77,7 @@ def generate_stable_response(prompt_input):
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run("stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4", 
-                             input={"width": 768, "height": 768, "prompt": "an astronaut riding a horse on mars, hd, dramatic lighting", "scheduler": "K_EULER",
-                              "num_outputs": 1, "guidance_scale": 7.5, "num_inference_steps": 50 , "num_inference_steps": 50  } )
+                             input= {"prompt": prompt_input}) 
     output = requests.get(output)
     output = Image.open(BytesIO(output.content))
     return str(output).replace('[','').replace(']','').replace("'","")
